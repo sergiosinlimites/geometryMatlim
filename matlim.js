fc7eac3891aa8216abbfdb0df2db09ea36307460
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2021-08-07 19:30:46 -0500

    the final logic was finished, the only thing that is left is the design

--- matlim.js
+++ matlim.js
@@ -1,18 +1,32 @@
 document.getElementById("square-option").addEventListener("click", showSquare);
 document.getElementById("rectangle-option").addEventListener("click", showRectangle);
 document.getElementById("triangle-option").addEventListener("click", showTriangle);
+document.getElementById("circle-option").addEventListener("click", showCircle);
 const squareChoosed = document.getElementById("square");
 const rectangleChoosed = document.getElementById("rectangle");
 const triangleChoosed = document.getElementById("triangle");
+const circleChoosed = document.getElementById("circle");
 const divAngleDegrees = document.getElementById("hyp-opp-adj");
 let angleDegreesInput = document.getElementById("hyp-opp-adj-angle-degrees");
+let secondaryHyp = document.getElementById("secondary-hyp");
+let secondaryOpp = document.getElementById("secondary-opp");
+let secondaryAdj = document.getElementById("secondary-adj");
 let sideToDiscover;
+let sideUserKnows;
+let result;
 let showKnownSide = document.getElementById("continue-known-side");
 let knownSide = document.getElementById("known-side");
 let showKnownSideValueDiv = document.getElementById("known-side-value-div")
 let labelKnownSide = document.getElementById("known-side-value-div__label");
 let valueKnownSide = document.getElementById("hyp-opp-adj-side-value");
 let solverButton = document.getElementById("solver-button");
+let diameterDiv = document.getElementById("diameter-div");
+let perimeterDiv = document.getElementById("perimeter-div");
+let areaDiv = document.getElementById("area-div");
+let volumeDiv = document.getElementById("volume-div");
+let hypDiv = document.getElementById("hyp-div");
+let oppDiv = document.getElementById("opp-div");
+let adjDiv = document.getElementById("adj-div");
 
 const pi = Math.PI;
 /* const preventReload = document.getElementsByClassName("button-submit");
@@ -37,42 +51,122 @@ document.getElementById("button-submit-square").addEventListener("click", functi
  */
 
 function showSquare(){
+    // show
     squareChoosed.classList.remove("not-shown");
     squareChoosed.classList.add("show-figure-block");
-    // don't show
+    areaDiv.classList.remove("not-shown");
+    areaDiv.classList.add("show-figure-inline");
+    perimeterDiv.classList.remove("not-shown");
+    perimeterDiv.classList.add("show-figure-inline");
+    // hide
     rectangleChoosed.classList.remove("show-figure-block");
     rectangleChoosed.classList.add("not-shown");
     triangleChoosed.classList.remove("show-figure-block");
     triangleChoosed.classList.add("not-shown");
+    circleChoosed.classList.remove("show-figure-block");
+    circleChoosed.classList.add("not-shown");
+    diameterDiv.classList.remove("show-figure-inline");
+    diameterDiv.classList.add("not-shown");
+    volumeDiv.classList.remove("show-figure-inline");
+    volumeDiv.classList.add("not-shown");
+    hypDiv.classList.remove("show-figure-inline");
+    hypDiv.classList.add("not-shown");
+    oppDiv.classList.remove("show-figure-inline");
+    oppDiv.classList.add("not-shown");
+    adjDiv.classList.remove("show-figure-inline");
+    adjDiv.classList.add("not-shown");
 }
 
 function showRectangle(){
     rectangleChoosed.classList.remove("not-shown");
     rectangleChoosed.classList.add("show-figure-block");
-    // don't show
+    areaDiv.classList.remove("not-shown");
+    areaDiv.classList.add("show-figure-inline");
+    perimeterDiv.classList.remove("not-shown");
+    perimeterDiv.classList.add("show-figure-inline");
+    // hide
     squareChoosed.classList.remove("show-figure-block");
     squareChoosed.classList.add("not-shown");
     triangleChoosed.classList.remove("show-figure-block");
     triangleChoosed.classList.add("not-shown");
+    circleChoosed.classList.remove("show-figure-block");
+    circleChoosed.classList.add("not-shown");
+    triangleChoosed.classList.remove("show-figure-block");
+    triangleChoosed.classList.add("not-shown");
+    circleChoosed.classList.remove("show-figure-block");
+    circleChoosed.classList.add("not-shown");
+    diameterDiv.classList.remove("show-figure-inline");
+    diameterDiv.classList.add("not-shown");
+    volumeDiv.classList.remove("show-figure-inline");
+    volumeDiv.classList.add("not-shown");
+    hypDiv.classList.remove("show-figure-inline");
+    hypDiv.classList.add("not-shown");
+    oppDiv.classList.remove("show-figure-inline");
+    oppDiv.classList.add("not-shown");
+    adjDiv.classList.remove("show-figure-inline");
+    adjDiv.classList.add("not-shown");
 }
 
 function showTriangle(){
+    // show
     triangleChoosed.classList.remove("not-shown");
     triangleChoosed.classList.add("show-figure-block");
-    // don't show
+    hypDiv.classList.add("show-figure-inline");
+    hypDiv.classList.remove("not-shown");
+    oppDiv.classList.add("show-figure-inline");
+    oppDiv.classList.remove("not-shown");
+    adjDiv.classList.add("show-figure-inline");
+    adjDiv.classList.remove("not-shown");
+    // hide
     squareChoosed.classList.remove("show-figure-block");
     squareChoosed.classList.add("not-shown");
     rectangleChoosed.classList.remove("show-figure-block");
     rectangleChoosed.classList.add("not-shown");
+    circleChoosed.classList.remove("show-figure-block");
+    circleChoosed.classList.add("not-shown");
+    areaDiv.classList.remove("show-figure-inline");
+    areaDiv.classList.add("not-shown");
+    perimeterDiv.classList.remove("show-figure-inline");
+    perimeterDiv.classList.add("not-shown");
+    diameterDiv.classList.remove("show-figure-inline");
+    diameterDiv.classList.add("not-shown");
+    volumeDiv.classList.remove("show-figure-inline");
+    volumeDiv.classList.add("not-shown");
 }
 
+function showCircle(){
+    // show
+    circleChoosed.classList.remove("not-shown");
+    circleChoosed.classList.add("show-figure-block");
+    areaDiv.classList.remove("not-shown");
+    areaDiv.classList.add("show-figure-inline");
+    perimeterDiv.classList.remove("not-shown");
+    perimeterDiv.classList.add("show-figure-inline");
+    diameterDiv.classList.remove("not-shown");
+    diameterDiv.classList.add("show-figure-inline");
+    // hide
+    squareChoosed.classList.remove("show-figure-block");
+    squareChoosed.classList.add("not-shown");
+    rectangleChoosed.classList.remove("show-figure-block");
+    rectangleChoosed.classList.add("not-shown");
+    triangleChoosed.classList.remove("show-figure-block");
+    triangleChoosed.classList.add("not-shown");
+    perimeterDiv.classList.remove("show-figure-inline");
+    perimeterDiv.classList.add("not-shown");
+    volumeDiv.classList.remove("show-figure-inline");
+    volumeDiv.classList.add("not-shown");
+    hypDiv.classList.remove("show-figure-inline");
+    hypDiv.classList.add("not-shown");
+    oppDiv.classList.remove("show-figure-inline");
+    oppDiv.classList.add("not-shown");
+    adjDiv.classList.remove("show-figure-inline");
+    adjDiv.classList.add("not-shown");
+}
 
 function squarePerimeter(width){
     return width * 4;
 }
-// console.log("El perímetro del cuadrado es: " + perimetroCuadrado + "cm");
 
-/* const areaCuadrado */ 
 function squareArea(width){
     return width**2;
 }
@@ -89,10 +183,10 @@ function trianglePerimeter(lado1, lado2, base){
     return lado1 + lado2 + base
 }
 
-function triangleArea(base, lado1){
+/* function triangleArea(base, lado1){
     const alturaTriangulo = Math.sqrt((lado1 ** 2) - ((base/2) ** 2));
     return parseFloat(((alturaTriangulo * base)/2).toFixed(4));
-}
+} */
 
 function circleDiameter(radio){
     return radio * 2
@@ -109,78 +203,128 @@ function circleArea(radio){
 }
 
 function calculateSquare(){
-    // pruebas
-    const validation1 = document.getElementById("square-decimals");
-    const validation2 = document.getElementById("square-width");
-    validation1.reportValidity(); // esto permite revisar si el elemento tiene todo lo necesario para ser pasado
-    validation2.reportValidity();
+    // requirements
+    const validation1 = document.getElementById("square-decimals").reportValidity();
+    const validation2 = document.getElementById("square-width").reportValidity();
+    // esto permite revisar si el elemento tiene todo lo necesario para ser pasado
 
-    const inputWidthValue = document.getElementById("square-width").value
-    const decimalsNumber = document.getElementById("square-decimals").value;
-    const perimeter = parseFloat(squarePerimeter(inputWidthValue).toFixed(decimalsNumber));
-    const area = parseFloat(squareArea(inputWidthValue).toFixed(decimalsNumber));
-    const writePerimeter = document.getElementById("perimeter-text");
-    const writeArea = document.getElementById("area-text");
-    const unitSelected = document.getElementById("square-unit-selected").value;
-    writePerimeter.textContent = perimeter + " " + unitSelected;
-    writeArea.textContent = area + " " + unitSelected + "^2";
+    if(validation1 === true && validation2 === true){
+        const inputWidthValue = document.getElementById("square-width").value
+        const decimalsNumber = document.getElementById("square-decimals").value;
+        const perimeter = parseFloat(squarePerimeter(inputWidthValue).toFixed(decimalsNumber));
+        const area = parseFloat(squareArea(inputWidthValue).toFixed(decimalsNumber));
+        // write
+        const writePerimeter = document.getElementById("perimeter-text");
+        const writeArea = document.getElementById("area-text");
+        const unitSelected = document.getElementById("square-unit-selected").value;
+        writePerimeter.textContent = perimeter + " " + unitSelected;
+        writeArea.textContent = area + " " + unitSelected + "^2";
+    }
 }
 
 function calculateRectangle(){
     // const validation1 = document.getElementById("rectangle-decimals");
-    const validation2 = document.getElementById("rectangle-length");
-    const validation3 = document.getElementById("rectangle-width");
-    // validation1.reportValidity(); // esto permite revisar si el elemento tiene todo lo necesario para ser pasado
-    validation2.reportValidity();
-    validation3.reportValidity();
-
-    const inputLengthValue = document.getElementById("rectangle-length").value;
-    const inputWidthValue = document.getElementById("rectangle-width").value;
-    let decimalsNumber = document.getElementById("rectangle-decimals").value;
-
-    
-    if(decimalsNumber > 10){
-        decimalsNumber = 10;
+    const validation2 = document.getElementById("rectangle-length").reportValidity()
+    const validation3 = document.getElementById("rectangle-width").reportValidity()
+    // esto permite revisar si el elemento tiene todo lo necesario para ser pasado
+    if(validation1 === true && validation2 === true){
+        const inputLengthValue = document.getElementById("rectangle-length").value;
+        const inputWidthValue = document.getElementById("rectangle-width").value;
+        let decimalsNumber = document.getElementById("rectangle-decimals").value;
+        // write
+        const perimeter = parseFloat(rectanglePerimeter(inputLengthValue, inputWidthValue).toFixed(decimalsNumber));
+        const area = parseFloat(rectangleArea(inputLengthValue, inputWidthValue).toFixed(decimalsNumber));
+        const writePerimeter = document.getElementById("perimeter-text");
+        const writeArea = document.getElementById("area-text");
+        const unitSelected = document.getElementById("rectangle-unit-selected").value;
+        writePerimeter.textContent = perimeter + " " + unitSelected;
+        writeArea.textContent = area + " " + unitSelected;
     }
-    
-    const perimeter = parseFloat(rectanglePerimeter(inputLengthValue, inputWidthValue).toFixed(decimalsNumber));
-    const area = parseFloat(rectangleArea(inputLengthValue, inputWidthValue).toFixed(decimalsNumber));
-    const writePerimeter = document.getElementById("perimeter-text");
-    const writeArea = document.getElementById("area-text");
-    const unitSelected = document.getElementById("rectangle-unit-selected").value;
-    writePerimeter.textContent = perimeter + " " + unitSelected;
-    writeArea.textContent = area + " " + unitSelected;
+}
 
+function calculateCircle(){
+    // requirements
+    const validation1 = document.getElementById("circle-decimals").reportValidity(); 
+    const validation2 = document.getElementById("circle-radio").reportValidity();
+    // esto permite revisar si el elemento tiene todo lo necesario para ser pasado
+    if(validation1 === true && validation2 === true){
+        const inputRadioValue = document.getElementById("circle-radio").value
+        let decimalsNumber = document.getElementById("circle-decimals").value;
+        const diameter = parseFloat(circleDiameter(inputRadioValue).toFixed(decimalsNumber));
+        const perimeter = parseFloat(circlePerimeter(inputRadioValue).toFixed(decimalsNumber));
+        const area = parseFloat(circleArea(inputRadioValue).toFixed(decimalsNumber));
+        // write
+        const writeDiameter = document.getElementById("diameter-text");
+        const writePerimeter = document.getElementById("perimeter-text");
+        const writeArea = document.getElementById("area-text");
+        const unitSelected = document.getElementById("square-unit-selected").value;
+        writeDiameter.textContent = diameter + " " + unitSelected;
+        writePerimeter.textContent = perimeter + " " + unitSelected;
+        writeArea.textContent = area + " " + unitSelected + "^2";
+    } 
 }
 
 function selectHypothenuse(){
+    // logic
+    angleDegreesInput.value = "";
+    valueKnownSide.value = "";
+    sideToDiscover = "hypothenuse";
     // show
     divAngleDegrees.classList.remove("not-shown");
     divAngleDegrees.classList.add("shown-figure-block");
-    angleDegreesInput.value = "";
-    sideToDiscover = "hypothenuse";
     showKnownSide.classList.remove("not-shown");
     showKnownSide.classList.remove("show-figure-inline-block");
+    secondaryOpp.classList.remove("not-show");
+    secondaryOpp.classList.add("show-figure-inline-block");
+    secondaryAdj.classList.remove("not-show");
+    secondaryAdj.classList.add("show-figure-inline-block");
+    // hide
+    knownSide.classList.remove("show-figure-block");
+    knownSide.classList.add("not-shown");
+    secondaryHyp.classList.remove("show-figure-inline-block");
+    secondaryHyp.classList.add("not-shown");
 }
 
 function selectOpposite(){
+    // logic
+    angleDegreesInput.value = "";
+    valueKnownSide.value = "";
+    sideToDiscover = "opposite side";
     // show
     divAngleDegrees.classList.remove("not-shown");
     divAngleDegrees.classList.add("shown-figure-block");
-    angleDegreesInput.value = "";
-    sideToDiscover = "opposite side";
     showKnownSide.classList.remove("not-shown");
     showKnownSide.classList.remove("show-figure-inline-block");
+    secondaryHyp.classList.remove("not-show");
+    secondaryHyp.classList.add("show-figure-inline-block");
+    secondaryAdj.classList.remove("not-show");
+    secondaryAdj.classList.add("show-figure-inline-block");
+    // hide
+    knownSide.classList.remove("show-figure-block");
+    knownSide.classList.add("not-shown");
+    secondaryOpp.classList.remove("show-figure-inline-block");
+    secondaryOpp.classList.add("not-shown");
 }
 
 function selectAdjacent(){
+    // logic
+    angleDegreesInput.value = "";
+    valueKnownSide.value = "";
+    sideToDiscover = "adjacent side";
     // show
     divAngleDegrees.classList.remove("not-shown");
     divAngleDegrees.classList.add("shown-figure-block");
-    angleDegreesInput.value = "";
-    sideToDiscover = "adjacent side";
     showKnownSide.classList.remove("not-shown");
     showKnownSide.classList.remove("show-figure-inline-block");
+    secondaryHyp.classList.remove("not-show");
+    secondaryHyp.classList.add("show-figure-inline-block");
+    secondaryOpp.classList.remove("not-show");
+    secondaryOpp.classList.add("show-figure-inline-block");
+    // hide
+    knownSide.classList.remove("show-figure-block");
+    knownSide.classList.add("not-shown");
+    secondaryAdj.classList.remove("show-figure-inline-block");
+    secondaryAdj.classList.add("not-shown");
 }
 
 function selectKnownSide(){
@@ -194,6 +338,7 @@ function selectKnownSide(){
 
 function selectSecondHyp(){
     valueKnownSide.value = "";
+    sideUserKnows = "hypothenuse";
     showKnownSideValueDiv.classList.remove("not-shown");
     showKnownSideValueDiv.classList.add("show-figure-block");
     labelKnownSide.textContent = "Hypothenuse value:";
@@ -203,17 +348,22 @@ function selectSecondHyp(){
 
 function selectSecondOpp(){
     valueKnownSide.value = "";
+    sideUserKnows = "opposite side";
     showKnownSideValueDiv.classList.remove("not-shown");
     showKnownSideValueDiv.classList.add("show-figure-block");
     labelKnownSide.textContent = "Opposite side value:";
-    
+    solverButton.classList.remove("not-shown");
+    solverButton.classList.add("show-figure-inline-block");
 }
 
 function selectSecondAdj(){
     valueKnownSide.value = "";
+    sideUserKnows = "adjacent side";
     showKnownSideValueDiv.classList.remove("not-shown");
     showKnownSideValueDiv.classList.add("show-figure-block");
     labelKnownSide.textContent = "Adjacent side value:";
+    solverButton.classList.remove("not-shown");
+    solverButton.classList.add("show-figure-inline-block");
 }
 
 function calcHypOppAdj(){
@@ -221,13 +371,46 @@ function calcHypOppAdj(){
     if(validation1 === true){
         switch(sideToDiscover){
             case "hypothenuse":
-                console.log("you are calc the hyp");
+                switch(sideUserKnows){
+                    case "opposite side":
+                        //console.log("The known side value is: " + valueKnownSide.value);
+                        //console.log("The angle degrees value is: " + angleDegreesInput.value);
+                        toRAD = angleDegreesInput.value / (180 / Math.PI);
+                        console.log("final: " + (valueKnownSide.value / Math.sin(toRAD)));
+                        break;
+                    case "adjacent side":
+                        toRAD = angleDegreesInput.value / (180 / Math.PI);
+                        console.log("final: " + (valueKnownSide.value / Math.cos(toRAD)));
+                        break;
+                    default:
+                        console.log("You did something wrong");
+                }
                 break;
             case "opposite side":
-                console.log("you are calc the opp");
+                switch(sideUserKnows){
+                    case "hypothenuse":
+                        toRAD = angleDegreesInput.value / (180 / Math.PI);
+                        console.log("final: " + (valueKnownSide.value * Math.sin(toRAD)));
+                        break;
+                    case "adjacent side":
+                        toRAD = angleDegreesInput.value / (180 / Math.PI);
+                        console.log("final: " + (valueKnownSide.value * Math.tan(toRAD)));
+                        break;
+                    default:
+                        console.log("You did something wrong");
+                }
                 break;
             case "adjacent side":
-                console.log("you are calc the adj");
+                switch(sideUserKnows){
+                    case "hypothenuse":
+                        toRAD = angleDegreesInput.value / (180 / Math.PI);
+                        console.log("final: " + (valueKnownSide.value * Math.cos(toRAD)));
+                        break;
+                    case "opposite side":
+                        toRAD = angleDegreesInput.value / (180 / Math.PI);
+                        console.log("final: " + (valueKnownSide.value / Math.tan(toRAD)));
+                        break;
+                }
                 break;
         }
     }
